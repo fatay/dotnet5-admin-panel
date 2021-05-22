9212b99317849c75fb800e95f24d7f85577883b8
Fatih Aydın <30989447+fatay@users.noreply.github.com>
2021-05-22 20:58:40 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual bool IsDeleted { get; set; } = false; // Henüz silinmemiş bir entity
+        public virtual bool isActive { get; set; } = true; // Durum.Aktif.Value => entity aktif
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;  //override edilebilir şekilde oluşturuyoruz (virtual).
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now; //override edilebilir şekilde oluşturuyoruz (virtual).
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get; set; }
+    }
+
+
+}

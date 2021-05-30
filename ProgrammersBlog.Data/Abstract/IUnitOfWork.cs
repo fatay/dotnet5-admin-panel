e5ddb7b7e16ec066388264a8057601be260e5f05
Fatih Aydın <30989447+fatay@users.noreply.github.com>
2021-05-30 12:06:22 +0300

    Add flow control with Unit of Work Pattern.

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    // Garbage Collector için IDisposable implement ediyoruz.
+    public interface IUnitOfWork:IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }    // unitOfWork.Articles diyerek erişilebileceğiz.
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }    // _unitOfWork.Categories.AddAsync();
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+
+        // Commit and Save Changes (Kaydet)
+        // _unitOfWork.Categories.DeleteAsync();
+        // _unitOfWork.Users.AddAsync();
+        // _unitOfWork.SaveAsync(); ---> Değişiklikleri kaydet.
+        Task<int> SaveAsync();                  
+    }
+}
